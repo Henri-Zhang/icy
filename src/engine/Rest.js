eabe24e41ad10d26b6c3b41e32d065a11a844ca7
barrow.zhang <371595867@qq.com>
2018-08-19 09:06:57 +0800

    Modify api url in Rest.

--- src/engine/Rest.js
+++ src/engine/Rest.js
@@ -1,12 +1,12 @@
 import axios from 'axios'
 import _ from 'lodash'
 
-// const api = 'https://www.easy-mock.com/mock/5b78bbc8c8ce472d34912f4a'
+const api = 'https://www.easy-mock.com/mock/5b78bbc8c8ce472d34912f4a'
 
 export const init = () => {
   axios.interceptors.request.use((config = {}) => {
     _.merge(config, {
-      url: `/api${config.url}`,
+      url: api + config.url,
       headers: {
         'Content-Type': 'application/json'
       }
